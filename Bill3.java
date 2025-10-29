030e0a6923d038855e669fb90a6f628ca4979a92
Lior Tzur <liortzur@Liors-MacBook-Air.local>
2025-10-29 20:18:04 +0200

    HW1 assignments update with correct email

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,10 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name1 = args[1];
+		String name1 = args[2];
 	    // Replace this comment with the rest of your code   
+		System.out.println(" there are " + numdiners +
+							 " diners around the table.");
 	}
 }
