191e0c1ad3fa9daaf123c3e0792054c592e96d25
Lior Tzur <liortzur@Liors-MacBook-Air.local>
2025-10-30 18:09:39 +0200

    update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -2,11 +2,26 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
+
+		// you arrive to the place with your freinds 
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-	    // Replace this comment with the rest of your code   
-		System.out.println(" there are " + numdiners +
-							 " diners around the table.");
+		
+		//you r 3 people
+		int numdiners = 3;
+
+		//you get the bill 
+		int bill = args[3];
+		
+		//calulations in the "back"
+		int payment = bill / numdiners;
+
+		//you wanna do the payment
+		System.out.println("okay thanks for eating here ");
+		System.out.println("we are " + numdiners + name1 + name2 + name3 );
+		System.out.println("you total" + bill );
+		System.out.println("each has to pay " + payment );
+
 	}
 }
