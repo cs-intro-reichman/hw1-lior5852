825048e8138dfdb9d8dbf45e3bf5381c1890eb18
Lior Tzur <liortzur@Liors-MacBook-Air.local>
2025-10-31 12:44:19 +0200

    fix2

--- FVCalc.java
+++ FVCalc.java
@@ -6,20 +6,21 @@ public class FVCalc {
 		int currentvalue = Integer.parseInt(args[0]);
 
 		// the annual intrest rate
-		double rate =Double.parseDouble(args[1]) / 100; 
+		double rate =Double.parseDouble(args[1]); 
 
 		// the number of years
 		int years = Integer.parseInt(args[2]);
 
-		// the totall 2 diffrent opertaions
+		// the totall with 2 diffrent opertaions
 		double a = (1 + rate);
 
 		double b = Math.pow(a,years);
 
+		// the totall
 
 		int totall = (int)(currentvalue * b);
 
-		System.out.println(" After 2 years, " + currentvalue + " saved at " + rate + "%" + " will yield " + totall + "$");
+		System.out.println(" After " + years + " years , " + currentvalue + " saved at " + rate + "%" + " will yield " + totall + "$");
 		
 	}
 }
