a05b3e8e2deda66aef99809bea2f881c2f8ef5a3
Lior Tzur <liortzur@Liors-MacBook-Air.local>
2025-11-03 08:32:22 +0200

    another one

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,22 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+	
+
+	// משתנה יחיד כי ארגומנט זה על כל ״קובייה״
+	int num = Integer.parseInt(args[0]);
+
+	// כדי להגיע למספר מאות אז נחלק ב100
+	int houndreds = num/100; 
+	num = num - (houndreds*100);
+
+	// כדי להגיע למספר מאות אז נחלק ב10
+	int tens = num/10;
+	num = num - (tens*10);
+
+	// זו השארית הסופית
+	int ones = num;
+
+	System.out.println(houndreds + " hundreds," + tens + " tens, and " + ones + " ones.");
 	}
 }
